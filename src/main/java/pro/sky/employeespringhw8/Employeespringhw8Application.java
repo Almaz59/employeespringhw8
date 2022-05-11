859c56d9b14210761082abd1009bcffcf0441725
Almaz59 <100568607+Almaz59@users.noreply.github.com>
2022-05-11 22:47:44 +0300

    hw8 first commit

---
+++ src/main/java/pro/sky/employeespringhw8/Employeespringhw8Application.java
@@ -0,0 +1,13 @@
+package pro.sky.employeespringhw8;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Employeespringhw8Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Employeespringhw8Application.class, args);
+    }
+
+}
---
+++ src/test/java/pro/sky/employeespringhw8/Employeespringhw8ApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.employeespringhw8;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Employeespringhw8ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
